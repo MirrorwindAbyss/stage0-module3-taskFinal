5e7f3cc35660c3703ee945096713ac774359ff90
Nabiyev Abdumalik <abdumalikunusov06@gmail.com>
2023-07-07 20:15:25 +0500

    Done!

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int first, second, third, fourth;
+        first = number%1000;
+        second = number/100%10;
+        third = number/10%10;
+        fourth = number%10;
+        System.out.println(fourth +""+ third +""+ second +""+ first);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first, second, third;
+        first = number/100;
+        second = number/10%10;
+        third = number%10;
+        System.out.println(third + "" + second + "" + first);
     }
 }
