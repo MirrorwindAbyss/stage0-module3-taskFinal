c7c8d095601d84c6f8e9d1631191d6c42da1414a
Nabiyev Abdumalik <abdumalikunusov06@gmail.com>
2023-07-09 11:18:06 +0500

    Done!

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -3,7 +3,7 @@ package lang.print.gaps.finalModuleTask;
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
         float temperatureFahrenheit;
-        temperatureFahrenheit = ((temperatureCelsius *9/5)+32);
+        temperatureFahrenheit = (float) ((temperatureCelsius *1.8)+32);
         System.out.println(temperatureFahrenheit);
     }
 }
